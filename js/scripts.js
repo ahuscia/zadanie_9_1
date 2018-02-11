f0e0eb206451ca9aaba24f0fa833c46f049abe35
ahuscia <a.huscia@gmail.com>
2018-02-11 11:44:47 +0100

    Change script.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 
 function getTriangleArea(a, h) {
 
-    if ( a, h > 0 ) {
+    if ( a > 0 && h > 0) {
         alert(a * h / 2);
         return(a * h / 2);
     } else {
@@ -15,5 +15,5 @@ console.log(getTriangleArea(10, 6))
 
 var triangle1Area = getTriangleArea(10, 15);
 var triangle2Area = getTriangleArea(0, 0);
-var triangleArea = getTriangleArea(12, 17);
+var triangleArea = getTriangleArea(12, 0);
 
