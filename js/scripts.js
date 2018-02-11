4a9f159bbabae10a69b38dd73037493d884dce5f
ahuscia <a.huscia@gmail.com>
2018-02-11 10:55:12 +0100

    Add index.html and script.js

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+// scripts.js
+
+function getTriangleArea(a, h) {
+
+    if ( a, h > 0 ) {
+        alert(a * h / 2);
+        return(a * h / 2);
+    } else {
+        alert('nieprawidłowe dane');
+        return('nieprawidłowe dane');
+    }
+}
+
+console.log(getTriangleArea(10, 6))
+
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(0, 0);
+var triangleArea = getTriangleArea(12, 17);
+
